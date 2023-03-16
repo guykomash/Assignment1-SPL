f421f79659bcea4798be3866ec14d6f1887de0da
guykomash <gkomash@gmail.com>
2023-03-16 21:59:46 +0000

    Assignmen1-SPL

---
+++ bin/Customer.d
@@ -0,0 +1,4 @@
+bin/Customer.o: src/Customer.cpp src/../include/Customer.h \
+ src/../include/Workout.h
+src/../include/Customer.h:
+src/../include/Workout.h:
---
+++ bin/Trainer.d
@@ -0,0 +1,7 @@
+bin/Trainer.o: src/Trainer.cpp src/../include/Trainer.h \
+ src/../include/Customer.h src/../include/Workout.h \
+ src/../include/Customer.h
+src/../include/Trainer.h:
+src/../include/Customer.h:
+src/../include/Workout.h:
+src/../include/Customer.h:
